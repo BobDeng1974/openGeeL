9faad0da1a49a170d82462afa2ce6642191254cb
Rafael Theis <s9rathei@stud.uni-saarland.de>
2017-02-27 18:23:56 +0100

    Code cleanup in world information class

--- openGeeL/renderer/utility/worldinformation.h
+++ openGeeL/renderer/utility/worldinformation.h
@@ -3,7 +3,6 @@
 
 #include <vec3.hpp>
 #include <mat4x4.hpp>
-#include <list>
 #include <map>
 
 namespace geeL {
@@ -31,11 +30,10 @@ namespace geeL {
 	};
 
 
+	//Object that can request and is dependend on world information 
 	class WorldInformationRequester {
 
 	public:
-		WorldInformationRequester() {}
-
 		virtual void addWorldInformation(std::map<WorldMaps, unsigned int> maps,
 			std::map<WorldMatrices, const glm::mat4*> matrices,
 			std::map<WorldVectors, const glm::vec3*> vectors) = 0;
@@ -43,15 +41,15 @@ namespace geeL {
 	};
 
 
+	//Object that can provide world information to linked requesters
 	class WorldInformationProvider {
 
 	public:
-		WorldInformationProvider() {}
-
 		virtual void addRequester(WorldInformationRequester& requester) = 0;
 		virtual void linkInformation() const = 0;
 
 	};
+
 
 
 	inline WorldMaps operator|(WorldMaps a, WorldMaps b) {
