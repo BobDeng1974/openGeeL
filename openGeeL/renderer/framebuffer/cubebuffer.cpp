de6ce7f33319cfd3697103daca2946ce44a2142e
Rafael Theis <s9rathei@stud.uni-saarland.de>
2017-07-13 23:02:49 +0200

    Removed deprecated cube buffer method

--- openGeeL/renderer/framebuffer/cubebuffer.cpp
+++ openGeeL/renderer/framebuffer/cubebuffer.cpp
@@ -3,7 +3,7 @@
 
 namespace geeL {
 
-	CubeBuffer::CubeBuffer() {
+	CubeBuffer::CubeBuffer() : texture(nullptr) {
 		glGenFramebuffers(1, &info.fbo);
 		glGenRenderbuffers(1, &rbo);
 	}
@@ -12,22 +12,19 @@ namespace geeL {
 		remove();
 	}
 
-	void CubeBuffer::init(unsigned int resolution, unsigned int textureID) {
-		this->textureID = textureID;
-		info.width = resolution;
-		info.height = resolution;
+	void CubeBuffer::init(const TextureCube& texture) {
+		this->texture = &texture;
+
+		info.width = texture.getResolution();
+		info.height = texture.getResolution();
 
 		glBindFramebuffer(GL_FRAMEBUFFER, info.fbo);
 		glBindRenderbuffer(GL_RENDERBUFFER, rbo);
-		glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, resolution, resolution);
+		glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, texture.getResolution(), texture.getResolution());
 		glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, rbo);
 
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 		glBindRenderbuffer(GL_RENDERBUFFER, 0);
-	}
-
-	void CubeBuffer::init(const TextureCube& texture) {
-		init(texture.getResolution(), texture.getID());
 	}
 
 
@@ -36,7 +33,7 @@ namespace geeL {
 
 		FrameBuffer::bind(info.fbo);
 		for (unsigned int side = 0; side < 6; side++) {
-			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_CUBE_MAP_POSITIVE_X + side, textureID, 0);
+			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_CUBE_MAP_POSITIVE_X + side, texture->getID(), 0);
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 			drawCall();
@@ -50,7 +47,7 @@ namespace geeL {
 
 		FrameBuffer::bind(info.fbo);
 		for (unsigned int side = 0; side < 6; side++) {
-			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_CUBE_MAP_POSITIVE_X + side, textureID, mipLevel);
+			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_CUBE_MAP_POSITIVE_X + side, texture->getID(), mipLevel);
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 			drawCall(side);
@@ -70,7 +67,7 @@ namespace geeL {
 
 	std::string CubeBuffer::toString() const {
 		return "Cube buffer " + std::to_string(info.fbo) + 
-			"\n -- Texture " + std::to_string(textureID) + "\n";
+			"\n -- Texture " + std::to_string(texture->getID()) + "\n";
 	}
 
 }
