19cf7b8f13dff5b07d2100e80eea35291af5c3e4
Rafael Theis <s9rathei@stud.uni-saarland.de>
2017-07-29 20:32:00 +0200

    Added generic object wrapper classes that are thread safe

---
+++ openGeeL/application/application.cpp
@@ -0,0 +1,68 @@
+#include <glfw3.h>
+#include <thread>
+#include <chrono>
+#include <algorithm>
+#include <mutex>
+#include <functional>
+#include <iostream>
+#include "../renderer/utility/rendertime.h"
+#include "../renderer/renderer.h"
+#include "../renderer/window.h"
+#include "../renderer/inputmanager.h"
+#include "application.h"
+ 
+using namespace std;
+
+namespace geeL {
+
+	Application::Application(RenderWindow& window, InputManager& inputManager, Renderer& renderer)
+		: window(window), inputManager(inputManager), renderer(renderer) {
+
+		auto exit = [this](GLFWwindow* window, int key, int scancode, int action, int mode) { 
+			if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+				glfwSetWindowShouldClose(window, GL_TRUE);
+		};
+
+		inputManager.addCallback(exit);
+		inputManager.init(&window);
+	}
+
+	mutex inputLock;
+	bool close = false;
+
+	void Application::run() {
+		glfwMakeContextCurrent(0);
+		
+		thread renderThread([this]() { renderer.render(); });
+		
+		Time& time = Time();
+		Time& inner = Time();
+
+		long FPS = 10;
+
+		while (!window.shouldClose()) {
+			inner.reset();
+
+			inputLock.lock();
+			glfwPollEvents();
+			inputManager.update();
+			inputLock.unlock();
+
+			inner.update();
+			long currFPS = std::max(0L, FPS - inner.deltaTime());
+			this_thread::sleep_for(chrono::milliseconds(currFPS));
+
+			time.update();
+		}
+
+		close = true;
+
+		renderThread.join();
+		window.close();
+	}
+
+	bool Application::closing() {
+		return close;
+	}
+
+}
---
+++ openGeeL/application/objectwrapper.h
@@ -0,0 +1,158 @@
+#ifndef OBJECTWRAPPER_H
+#define OBJECTWRAPPER_H
+
+#include <atomic>
+#include <mutex>
+#include <iostream>
+
+namespace geeL {
+
+	template<typename T>
+	class TempWrapper {
+
+	public:
+		TempWrapper(T& obj);
+		TempWrapper(T& obj, std::recursive_mutex& mutex);
+		~TempWrapper();
+
+		T* operator->();
+
+	private:
+		T* obj;
+		std::recursive_mutex* mutex;
+
+	};
+
+
+
+	template<typename T>
+	class ObjectWrapper {
+
+	public:
+		ObjectWrapper(T& obj);
+
+		virtual TempWrapper<T> operator->();
+
+	protected:
+		T* obj;
+
+	};
+
+
+
+	template<typename T>
+	class MutexWrapper : public ObjectWrapper<T> {
+
+	public:
+		MutexWrapper(T& obj);
+
+		virtual TempWrapper<T> operator->();
+
+	private:
+		std::recursive_mutex mutex;
+
+	};
+
+
+	template<typename T>
+	class AtomicWrapper {
+
+	public:
+		AtomicWrapper();
+		AtomicWrapper(const T& obj);
+		AtomicWrapper(const AtomicWrapper<T>& other);
+
+		T operator()() const;
+		operator T() const;
+		AtomicWrapper<T>& operator=(const T& o);
+		AtomicWrapper<T>& operator=(const AtomicWrapper<T>& o);
+
+	private:
+		std::atomic<T> atom;
+
+
+	};
+
+
+
+	
+
+	template<typename T>
+	inline TempWrapper<T>::TempWrapper(T & obj) : obj(&obj), mutex(nullptr) {}
+
+	template<typename T>
+	inline TempWrapper<T>::TempWrapper(T& obj, std::recursive_mutex& mutex) : obj(&obj), mutex(&mutex) {
+		mutex.lock();
+	}
+
+	template<typename T>
+	inline TempWrapper<T>::~TempWrapper() {
+		if(mutex != nullptr) mutex->unlock();
+	}
+
+	template<typename T>
+	inline T* TempWrapper<T>::operator->() {
+		return obj;
+	}
+
+
+
+	template<typename T>
+	inline ObjectWrapper<T>::ObjectWrapper(T& obj) : obj(&obj) {}
+
+	template<typename T>
+	inline TempWrapper<T> ObjectWrapper<T>::operator->() {
+		return TempWrapper<T>(*obj);
+	}
+
+	template<typename T>
+	inline MutexWrapper<T>::MutexWrapper(T& obj) : ObjectWrapper<T>(obj) {}
+
+	template<typename T>
+	inline TempWrapper<T> MutexWrapper<T>::operator->() {
+		return TempWrapper<T>(*obj, mutex);
+	}
+
+	
+
+
+	template<typename T>
+	inline AtomicWrapper<T>::AtomicWrapper() : atom(T()) {}
+
+	template<typename T>
+	inline AtomicWrapper<T>::AtomicWrapper(const T& obj) : atom(obj) {}
+
+	template<typename T>
+	inline AtomicWrapper<T>::AtomicWrapper(const AtomicWrapper<T>& other) : atom(other()) {}
+
+
+	template<typename T>
+	inline T AtomicWrapper<T>::operator()() const {
+		return atom.load();
+	}
+
+	template<typename T>
+	inline AtomicWrapper<T>::operator T() const {
+		return atom.load();
+	}
+
+	template<typename T>
+	inline AtomicWrapper<T>& AtomicWrapper<T>::operator=(const T& o) {
+		std::cout << "ayy\n";
+		atom.store(o);
+
+		return *this;
+	}
+
+	template<typename T>
+	inline AtomicWrapper<T>& AtomicWrapper<T>::operator=(const AtomicWrapper<T>& o) {
+		std::cout << "lmao\n";
+		atom.store(o());
+
+		return *this;
+	}
+
+}
+
+
+#endif
