61a29a785a272f4fac6b0321a773523f62bff2c0
Rafael Theis <s9rathei@stud.uni-saarland.de>
2017-08-29 17:03:01 +0200

    Updated shared compute post effect example

--- openGeeL/renderer/shaders/computeshared.com.glsl
+++ openGeeL/renderer/shaders/computeshared.com.glsl
@@ -7,18 +7,29 @@ layout(binding = 1, rgba16f) uniform image2D source;
 
 uniform vec2 resolution;
 
-shared vec4 groupColor = vec4(0.f);
+shared vec4 groupColor;
+shared vec4 groupColors[8][8];
 
 void main() {
 	//if(gl_GlobalInvocationID.x >= resolution.x || gl_GlobalInvocationID.y >= resolution.y) return;
+	
 	ivec2 coords = ivec2(gl_GlobalInvocationID.xy);
+	groupColors[gl_LocalInvocationID.x][gl_LocalInvocationID.y] = imageLoad(source, coords);
 
-	vec4 c = imageLoad(source, coords);
-	groupColor = groupColor + c;
-
-	//memoryBarrierShared();
 	groupMemoryBarrier();	barrier();
 
-	vec4 col = groupColor;
-	imageStore(image, coords, col);
+	if(gl_LocalInvocationID.x == 0 && gl_LocalInvocationID.y == 0) {
+		groupColor = vec4(0.f);		
+		for (int x = 0; x < 8; x++) {
+			for (int y = 0; y < 8; y++) {
+				groupColor = groupColor + groupColors[x][y];
+			}
+		}
+
+		groupColor = groupColor / 64.f;
+	}
+
+	groupMemoryBarrier();	barrier();
+
+	imageStore(image, coords, groupColor);
 }
