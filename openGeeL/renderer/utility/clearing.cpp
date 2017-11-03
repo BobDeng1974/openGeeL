3b7458c5fea19be46d7dcc029af6c7cb093ecdef
Rafael Theis <rafael_ths@yahoo.de>
2017-11-03 19:57:24 +0100

    Optimized clearing method

--- openGeeL/renderer/utility/clearing.cpp
+++ openGeeL/renderer/utility/clearing.cpp
@@ -2,25 +2,18 @@
 #include <glew.h>
 #include "clearing.h"
 
+#include <iostream>
+
 namespace geeL {
 
 	void Clearer::clear() const {
 		if (i == 0) return;
 
-		if ((i & (unsigned int)ClearingMethod::Color) != 0) {
-			glClear(GL_COLOR_BUFFER_BIT);
-		}
-			
-		if ((i & (unsigned int)ClearingMethod::Depth) != 0) {
-			glClear(GL_DEPTH_BUFFER_BIT);
-		}
-			
-		if ((i & (unsigned int)ClearingMethod::Stencil) != 0) {
-			glClear(GL_STENCIL_BUFFER_BIT);
-		}
+		unsigned int clear = ((i & (unsigned int)ClearingMethod::Color) != 0) * GL_COLOR_BUFFER_BIT
+			+ ((i & (unsigned int)ClearingMethod::Depth) != 0) * GL_DEPTH_BUFFER_BIT
+			+ ((i & (unsigned int)ClearingMethod::Stencil) != 0) * GL_STENCIL_BUFFER_BIT;
 
+		glClear(clear);
 	}
-
-	
 
 }
