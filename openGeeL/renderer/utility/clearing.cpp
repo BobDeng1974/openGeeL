1ea6d9ac20bd4a307a24fc09449d193d240816a9
Rafael Theis <rafael_ths@yahoo.de>
2017-11-05 16:17:18 +0100

    Clearer now resets color every clearing call

--- openGeeL/renderer/utility/clearing.cpp
+++ openGeeL/renderer/utility/clearing.cpp
@@ -7,12 +7,14 @@ namespace geeL {
 	void Clearer::clear() const {
 		if (i == 0) return;
 
+		bool clearColor   = (i & (unsigned int)ClearingMethod::Color) != 0;
 		bool clearStencil = (i & (unsigned int)ClearingMethod::Stencil) != 0;
 
-		unsigned int clear = ((i & (unsigned int)ClearingMethod::Color) != 0) * GL_COLOR_BUFFER_BIT
+		unsigned int clear = clearColor * GL_COLOR_BUFFER_BIT
 			+ ((i & (unsigned int)ClearingMethod::Depth) != 0) * GL_DEPTH_BUFFER_BIT
 			+ clearStencil * GL_STENCIL_BUFFER_BIT;
 
+		if(clearColor) glClearColor(0.0001f, 0.0001f, 0.0001f, 1.f);
 		if(clearStencil) glStencilMask(0xFF);
 		glClear(clear);
 	}
