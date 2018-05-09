eb55f5b50c796241f68877bbe111c4721fdee637
Rafael Theis <s9rathei@stud.uni-saarland.de>
2018-05-09 10:42:20 +0200

    Made render texture a functional texture

--- openGeeL/renderer/texturing/rendertexture.cpp
+++ openGeeL/renderer/texturing/rendertexture.cpp
@@ -3,38 +3,66 @@
 #include <cassert>
 #include "framebuffer/framebuffer.h"
 #include "rendertexture.h"
+#include <iostream>
 
 namespace geeL {
 	
-	RenderTexture::RenderTexture()
-		: Texture2D(ColorType::None) {}
+	RenderTexture::RenderTexture() {}
 	
 	RenderTexture::RenderTexture(Resolution resolution,
-		ColorType colorType, 
-		WrapMode wrapMode, 
-		FilterMode filterMode) 
-			: Texture2D(resolution, 
-				colorType, 
-				filterMode, 
+		ColorType colorType,
+		WrapMode wrapMode,
+		FilterMode filterMode)
+			: FunctionalTexture(std::unique_ptr<Texture>(new Texture2D(
+				resolution,
+				colorType,
+				filterMode,
 				wrapMode,
-				AnisotropicFilter::None,
-				0) {}
+				AnisotropicFilter::None, 0))) {}
 
-	void RenderTexture::setRenderResolution() const {
-		Texture2D::setRenderResolution();
+
+	void RenderTexture::applyRenderResolution() const {
+		getTexture().applyRenderResolution();
+	}
+
+	Resolution RenderTexture::getResolution() const {
+		return getTexture().getScreenResolution();
 	}
 
 	Resolution RenderTexture::getRenderResolution() const {
-		return Texture2D::getResolution();
+		return getTexture().getScreenResolution();
 	}
-
 
 	unsigned int RenderTexture::getSize() const {
 		return 1;
 	}
 
+	void RenderTexture::resize(Resolution resolution) {
+		getTexture().resize(resolution);
+	}
+
+	ColorType RenderTexture::getColorType() const {
+		return getTexture().getColorType();
+	}
+
+	ResolutionScale RenderTexture::getScale() const {
+		return getTexture().getScreenResolution().getScale();
+	}
+
+	void RenderTexture::attachParameters(const TextureParameters & parameters) {
+		getTexture().attachParameters(parameters);
+	}
+
+	void RenderTexture::detachParameters() {
+		getTexture().detachParameters();
+	}
+
+	void RenderTexture::mipmap() const {
+		getTexture().mipmap();
+	}
+
 	void RenderTexture::assign(unsigned int position) const {
-		Texture2D::assign(position);
+		getTexture().assign(position);
 	}
 
 }
--- openGeeL/renderer/texturing/rendertexture.h
+++ openGeeL/renderer/texturing/rendertexture.h
@@ -2,12 +2,13 @@
 #define RENDERTEXTURE_H
 
 #include "framebuffer/rendertarget.h"
+#include "functionaltexture.h"
 #include "texture.h"
 
 namespace geeL {
 
 	//Texture that is intended to be rendered into
-	class RenderTexture : public Texture2D, public RenderTarget {
+	class RenderTexture : public FunctionalTexture, public RenderTarget {
 
 	public:
 		RenderTexture();
@@ -16,9 +17,20 @@ namespace geeL {
 			WrapMode wrapMode = WrapMode::Repeat, 
 			FilterMode filterMode = FilterMode::None);
 
-		virtual void setRenderResolution() const;
+		virtual void applyRenderResolution() const;
+
+		Resolution getResolution() const;
 		virtual Resolution getRenderResolution() const;
 		virtual unsigned int getSize() const;
+		void resize(Resolution resolution);
+
+		ColorType getColorType() const;
+		ResolutionScale getScale() const;
+
+
+		void attachParameters(const TextureParameters& parameters);
+		void detachParameters();
+		void mipmap() const;
 
 	protected:
 		virtual void assign(unsigned int position) const;
